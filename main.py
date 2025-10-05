b82637155046b5fb9cbbf462025ca292c45ba1d8
Max I. <clickkava@gmail.com>
2025-10-05 20:11:45 +0300

    System prompt & Function declaration

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def main():
     if len(sys.argv) < 2:
@@ -27,13 +35,21 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
 
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    print(response.text)
+    for part in response.candidates[0].content.parts:
+        if part.function_call:
+            function_call_part = part.function_call
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        else:
+            print(part.text)
 
 
 if __name__ == "__main__":
